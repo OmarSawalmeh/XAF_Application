3b37e63d0f38474a5d1229ccbd2df84f029041d5
o.sawalmeh <o.sawalmeh@PC-179>
2023-02-19 17:17:26 +0300

    Add project files.

---
+++ XAF_Application.Module/BusinessObjects/Project.cs
@@ -0,0 +1,67 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.DC;
+using DevExpress.ExpressApp.Model;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.Validation;
+using DevExpress.Xpo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace XAF_Application.Module.BusinessObjects
+{
+    [DefaultClassOptions]
+
+    public class Project : BaseObject
+    { // Inherit from a different class to provide a custom primary key, concurrency and deletion behavior, etc. (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument113146.aspx).
+        // Use CodeRush to create XPO classes and properties with a few keystrokes.
+        // https://docs.devexpress.com/CodeRushForRoslyn/118557
+        public Project(Session session)
+            : base(session)
+        {
+        }
+        public override void AfterConstruction()
+        {
+            base.AfterConstruction();
+            // Place your initialization code here (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument112834.aspx).
+        }
+
+        string _projectName;
+        [Size(SizeAttribute.DefaultStringMappingFieldSize)]
+        public string ProjcetName
+        {
+            get => _projectName;
+            set => SetPropertyValue(nameof(ProjcetName), ref _projectName, value);
+        }
+
+        Employee _assignedTo;
+        [Association("Employee-Projects")]
+        public Employee AssignedTo
+        {
+            get => _assignedTo;
+            set => SetPropertyValue(nameof(AssignedTo), ref _assignedTo, value);
+        }
+
+        [Association("Project-ProjectTasks")]
+        public XPCollection<ProjectTask> ProjectTasks
+        {
+            get
+            {
+                return GetCollection<ProjectTask>(nameof(ProjectTasks));
+            }
+        }
+
+        Customer _customer;
+        [Association("Customer-Projects")]
+        public Customer Customer
+        {
+            get => _customer;
+            set => SetPropertyValue(nameof(Customer), ref _customer, value);
+        }
+
+    }
+}
---
+++ XAF_Application.Module/Controllers/Class1.cs
@@ -0,0 +1,46 @@
+﻿using DevExpress.DataProcessing.InMemoryDataProcessor;
+using DevExpress.ExpressApp.Actions;
+using DevExpress.XtraPrinting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using XAF_Application.Module.BusinessObjects;
+
+namespace XAF_Application.Module.Controllers
+{
+    public class TaskCompletedController : ViewController
+    {
+        SimpleAction Complete;
+
+        public TaskCompletedContoller()
+        {
+            TargetObjectType = typeof(ProjectTask);
+
+            Complete = new SimpleAction(this, "Complete", DevExpress.Persistent.Base.PredefinedCategory.Edit);
+            Complete.SelectionDependencyType = SelectionDependencyType.Independent;
+            Complete.Execute += Complete_Execute;
+
+        }
+
+        protected override void onViewControlsCreated()
+        {
+            base.onViewControlsCreated();
+        }
+
+        protected override void OnDeactivated()
+        {
+            base.OnDeactivated();
+        }
+
+        void Complete_Execute(object sender, SimpleActionExecuteEventArgs e) { 
+            var currentObj = e.CurrentObject as ProjectTask;
+            if(currentObj != null)
+            {
+
+            }
+         }
+    }
+}
